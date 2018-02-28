ffd41307b08de27f2eb1de614603b5ea4c31e623
Adam Lindberg <hello@alind.io>
2018-02-28 15:40:35 +0100

    Swagger generated code

--- py/tests/swagger_client/models/ping.py
+++ py/tests/swagger_client/models/ping.py
@@ -34,6 +34,7 @@ class Ping(object):
     """
     swagger_types = {
         'source': 'str',
+        'pub_key': 'str',
         'genesis_hash': 'str',
         'best_hash': 'str',
         'difficulty': 'float',
@@ -43,6 +44,7 @@ class Ping(object):
 
     attribute_map = {
         'source': 'source',
+        'pub_key': 'pub_key',
         'genesis_hash': 'genesis_hash',
         'best_hash': 'best_hash',
         'difficulty': 'difficulty',
@@ -50,10 +52,11 @@ class Ping(object):
         'peers': 'peers'
     }
 
-    def __init__(self, source=None, genesis_hash=None, best_hash=None, difficulty=None, share=None, peers=None):  # noqa: E501
+    def __init__(self, source=None, pub_key=None, genesis_hash=None, best_hash=None, difficulty=None, share=None, peers=None):  # noqa: E501
         """Ping - a model defined in Swagger"""  # noqa: E501
 
         self._source = None
+        self._pub_key = None
         self._genesis_hash = None
         self._best_hash = None
         self._difficulty = None
@@ -62,6 +65,7 @@ class Ping(object):
         self.discriminator = None
 
         self.source = source
+        self.pub_key = pub_key
         self.genesis_hash = genesis_hash
         self.best_hash = best_hash
         self.difficulty = difficulty
@@ -92,6 +96,29 @@ class Ping(object):
             raise ValueError("Invalid value for `source`, must not be `None`")  # noqa: E501
 
         self._source = source
+
+    @property
+    def pub_key(self):
+        """Gets the pub_key of this Ping.  # noqa: E501
+
+
+        :return: The pub_key of this Ping.  # noqa: E501
+        :rtype: str
+        """
+        return self._pub_key
+
+    @pub_key.setter
+    def pub_key(self, pub_key):
+        """Sets the pub_key of this Ping.
+
+
+        :param pub_key: The pub_key of this Ping.  # noqa: E501
+        :type: str
+        """
+        if pub_key is None:
+            raise ValueError("Invalid value for `pub_key`, must not be `None`")  # noqa: E501
+
+        self._pub_key = pub_key
 
     @property
     def genesis_hash(self):
